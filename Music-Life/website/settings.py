0d20df648aae1b50420602efbff3d6e8053e26ce
christianpongos <32566090+christianpongos@users.noreply.github.com>
2017-10-06 18:39:28 +0800

    Update settings.py

--- Music-Life/website/settings.py
+++ Music-Life/website/settings.py
@@ -57,8 +57,12 @@ WSGI_APPLICATION = 'website.wsgi.application'
 # Database
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'christianpongos0$default',
+        'USER': 'christianpongos0',
+        'PASSWORD': 'ikawnamalakas',
+        'HOST': 'christianpongos07.mysql.pythonanywhere-services.com'
+        
     }
 }
 
